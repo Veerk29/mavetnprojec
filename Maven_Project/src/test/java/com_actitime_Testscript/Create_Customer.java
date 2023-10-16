130436e7a8004e1eceb1f8ba83784e3d64eff2dd
Veeresh VK <Veeresh VK@VEER>
2023-10-16 14:06:48 +0530

    first commit

---
+++ Maven_Project/src/test/java/com_actitime_Testscript/Create_Customer.java
@@ -0,0 +1,15 @@
+package com_actitime_Testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Create_Customer {
+@Test
+public void createcustomer()
+{
+	Reporter.log("createcustomer",true);
+}
+
+
+}
+
---
+++ Maven_Project/src/test/java/com_actitime_Testscript/Create_Task.java
@@ -0,0 +1,11 @@
+package com_actitime_Testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Create_Task {
+@Test
+public void CreateTask() {
+	Reporter.log("createTask",true);
+	}
+}
