5ff080b774ceb3ce4b8f58ec34d6cf360803488c
Veeresh VK <Veeresh VK@VEER>
2023-10-16 14:36:52 +0530

    second commit

---
+++ Maven_Project/src/test/java/com_actitime_Testscript/maven_project.java
@@ -0,0 +1,13 @@
+package com_actitime_Testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class maven_project {
+@Test
+public void editcustomer()
+{
+	Reporter.log("hi");
+}
+}
